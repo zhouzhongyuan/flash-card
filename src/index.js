35611490c5ca8194eea93ca521c276d0f073052a
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2017-06-17 16:32:47 +0800

    Add IndexRoute

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { List, Quiz, Add } from './component';
 import './index.css';
 import App from './App';
@@ -10,6 +10,7 @@ injectTapEventPlugin();
 ReactDOM.render(
     <Router history={browserHistory} >
         <Route path="/" component={App}>
+            <IndexRoute component={List} />
             <Route path="list" component={List} />
             <Route path="add" component={Add} />
             <Route path="quiz" component={Quiz} />
